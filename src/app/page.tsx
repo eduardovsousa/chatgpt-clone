2f67081fff04cc3a0a47f97e0d9169c42c35ef51
eduardovsousa <eduardo.varela@outlook.com.br>
2023-06-01 01:21:16 -0300

    chore: update sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,35 @@
-'use client'
+"use client";
 
-import Sidebar from "@/components/Sidebar"
-import { useState } from "react"
+import Sidebar from "@/components/Sidebar";
+import { useState } from "react";
 
 const Page = () => {
   const [sideBarOpened, setSideBarOpened] = useState(false);
 
-  const closeSidebar = () => {}
+  const closeSidebar = () => {
+    setSideBarOpened(false);
+  };
+
+  const handleClearConversations = () => {};
+
+  const handleNewChat = () => {};
 
   return (
     <main className="flex min-h-screen bg-gpt-gray">
       <Sidebar
         open={sideBarOpened}
         onClose={closeSidebar}
+        onClear={handleClearConversations}
+        onNewChat={handleNewChat}
       >
-
+        
       </Sidebar>
+      <div className="w-16 h-screen">Teste</div>
       <section className="flex flex-col w-full">
-        ...
+        <button onClick={() => setSideBarOpened(true)}>Abr</button>
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
